8e5cf70a2552491d7e2607d3390e35796fc295fb
Harikesh2 <mths83478@gmail.com>
2023-11-21 21:43:16 +0530

    working on reactRouter

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <h1 className=' bg-green-400 text-blue-500'>Hello React</h1>
+   </>
+  )
+}
+
+export default App
