3316f640faebf5332a45f2ddd7414ac2606a3820
Harikesh2 <mths83478@gmail.com>
2023-11-22 17:17:56 +0530

    Completed the React Router

---
+++ reactRouter/src/component/Github/Github.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function GitHub (){
+    return(
+        <h1 className=" bg-green-400 text-center">Hello GitHub</h1>
+    )
+}
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './Index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './Root.jsx'
+import Home from './component/Home/Home.jsx'
+import About from './component/About/About.jsx'
+import Contact from './component/Contact/Contact.jsx'
+import User from './component/User/User.jsx'
+import GitHub from './component/Github/Github.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "user/:id",
+        element: <User />
+      },
+      {
+        path: "github",
+        element: <GitHub />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
