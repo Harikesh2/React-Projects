b4e4340d26a212f145391050d3124bb7e38e57e7
Harikesh2 <mths83478@gmail.com>
2023-12-15 20:49:12 +0530

    added redux in project

--- myBlogApp/src/App.jsx
+++ myBlogApp/src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import useDispatch from 'react-redux'
+import authservice from './appwrite/auth'
+import {login, logout} from './store/authSlice'
 
 
 function App() {
-  console.log();
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authservice.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
 
   return (
     <>
