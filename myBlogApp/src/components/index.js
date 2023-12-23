f3fdbb00593e73c2d22c1a3f3384b74a95a01485
Harikesh2 <mths83478@gmail.com>
2023-12-23 19:54:39 +0530

    added few more component!!

--- myBlogApp/src/components/index.js
+++ myBlogApp/src/components/index.js
@@ -3,11 +3,16 @@ import Footer from "./footer/footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/Logout";
-
+import Button from "./Button";
+import Postcard from "./Postcard";
+import Input from "./Input";
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Postcard,
+    Input
 }
