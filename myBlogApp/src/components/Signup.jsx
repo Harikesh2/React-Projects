b60ffe96c48aa5b93a7db21dab3d247d3d9b1dd8
Harikesh2 <mths83478@gmail.com>
2023-12-26 16:34:36 +0530

    added the file authLayout

--- myBlogApp/src/components/Signup.jsx
+++ myBlogApp/src/components/Signup.jsx
@@ -36,6 +36,50 @@ function Signup(){
                                 <Logo width="100%" />
                             </span>
                         </div>
+                        <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+                        <p className="mt-2 text-center text-base text-black/60">
+                            Already have an account?&nbsp;
+                            <Link
+                                to="/login"
+                                className="font-medium text-primary transition-all duration-200 hover:underline"
+                            >
+                                Sign In
+                            </Link>
+                        </p>
+                        {error &&  <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+                        <form onSubmit={handleSubmit(create)}>
+                            <div className=" space-y-5 ">
+                                <input 
+                                label="Full Name:"
+                                placeholder="Enter your Full Name"
+                                {...register("name",{
+                                    required: true,
+                                })}
+                                />
+                                   <Input
+                                        label="Email: "
+                                        placeholder="Enter your email"
+                                        type="email"
+                                        {...register("email", {
+                                            required: true,
+                                            validate: {
+                                                matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                                "Email address must be a valid address",
+                                            }
+                                        })}
+                                    />
+                                    <Input
+                                        label="Password: "
+                                        type="password"
+                                        placeholder="Enter your password"
+                                        {...register("password", {
+                                            required: true,})}
+                                    />
+                                    <button type="Submit"
+                                    className=" w-full">Create Account</button>
+                            </div>
+                        </form>
                 </div>
         </div>
     )
