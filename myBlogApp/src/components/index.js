8091d8df28724fbc5564a68f0919f92ee4fc3eb8
Harikesh2 <mths83478@gmail.com>
2024-01-01 16:15:37 +0530

    made changes in the files section!!

--- myBlogApp/src/components/index.js
+++ myBlogApp/src/components/index.js
@@ -25,5 +25,6 @@ export {
     Signup,
     Login,
     AuthLayout,
-    Select
+    Select,
+    PostForm,
 }
--- myBlogApp/src/pages/EditPost.jsx
+++ myBlogApp/src/pages/EditPost.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react";
-import { Container } from "../components";
+import { Container, PostForm } from "../components";
 import service from "../appwrite/config";
 import { useNavigate, useParams } from "react-router-dom";
-import {PostForm} from '../components/Post-form/PostForm';
+
 
 function EditPost(){
     const [post, SetPosts]= useState(null)
@@ -21,7 +21,7 @@ function EditPost(){
     },[slug, navigate])
     return post ? (<div className=" py-8">
         <Container>
-           <PostForm post={post} />
+          <PostForm post={post}/>
         </Container>
     </div>) : null
 
